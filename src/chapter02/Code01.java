5e551587049a90ae578899fcb12beb36a4feba72
qkrwnsqja <wnsgks0205@gmail.com>
2025-03-17 11:52:32 +0900

    main Method 사용법, 연결연산자, 덧셈연산자, println, print차이점

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+public class Code01 {
+    public static void main(String[] args) {
+        System.out.println("AISoftware");
+        System.out.println("Parkjun Beom");
+        System.out.printf("Grade 1\n");
+        System.out.println("result:" + 100 + 200);
+        System.out.println("result:" + (100 + 200));
+        System.out.println(100 + 200 + "won");
+    }
+}
