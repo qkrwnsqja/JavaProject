5e5a69929bae493fe3375b7591b747c537b71b78
qkrwnsqja <wnsgks0205@gmail.com>
2025-05-12 09:50:22 +0900

    6장 반복문- For2, 한줄에 N개씩 출력

---
+++ src/Chapter06/ForTest2.java
@@ -0,0 +1,11 @@
+package Chapter06;
+
+public class ForTest2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.print( i + 1 + "\t");
+            if((i+1) % 7 == 0)
+                System.out.println();
+        }
+    }
+}
