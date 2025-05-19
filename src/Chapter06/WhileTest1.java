b7353264f16f0d64136eb681e62ad78efe37934d
qkrwnsqja <wnsgks0205@gmail.com>
2025-05-19 09:21:39 +0900

    6장 반복문-for문과 같이 반복 횟수를 정해놓고 사용하는 경우

---
+++ src/Chapter06/WhileTest1.java
@@ -0,0 +1,12 @@
+package Chapter06;
+
+//for문과 같이 반복 횟수를 정해놓고 사용하는 경우
+public class WhileTest1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i + 1 + "번: 인공지능소프트웨어과");
+            i++;
+        }
+    }
+}
