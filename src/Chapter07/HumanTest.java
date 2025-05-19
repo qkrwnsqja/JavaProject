206a449e6c8978caf640faad3e83011444c00de0
qkrwnsqja <wnsgks0205@gmail.com>
2025-05-19 11:48:25 +0900

    7장 클래스와 객체

---
+++ src/Chapter07/HumanTest.java
@@ -0,0 +1,21 @@
+package Chapter07;
+
+public class HumanTest {
+    public static void main(String[] args) {
+        Human human1 = new Human();
+        Human human2 = new Human();
+
+        human1.name = "김폴리";
+        human1.age = 20;
+
+        human2.name = "홍인공";
+        human2.age = 23;
+
+        human1.walk("학생식당");
+        human2.eat("치킨가라오게");
+
+        human2.walk("이태원불꽃식당");
+        human2.eat("짜장면");
+
+    }
+}
