8c14ed7bfd97eef3a66f705430a00e0dc7e61b6d
qkrwnsqja <wnsgks0205@gmail.com>
2025-05-19 09:26:55 +0900

    6장 반복문-while문_무한루프

---
+++ src/Chapter06/WhileTest2.java
@@ -0,0 +1,9 @@
+package Chapter06;
+//무한루프
+public class WhileTest2 {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("AI");
+        }
+    }
+}
